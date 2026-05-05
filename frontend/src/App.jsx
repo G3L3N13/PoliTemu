d0be9c241fb88b0fd5ffdf77643ad433bed7a482
G3L3N13 <168616910+G3L3N13@users.noreply.github.com>
2026-05-05 16:17:29 -0500

    Estructura del proyecto

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+  return (
+    <main>
+      <h1>PoliTemu</h1>
+      <p>Login</p>
+    </main>
+  );
+}
+
+export default App;
+
+
